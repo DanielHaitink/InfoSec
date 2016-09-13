5a99131bc3cea447530d77c0da16af1c55225200
DanielsWrath <danielhaitink@gmail.com>
2016-09-14 00:22:00 +0200

    decrypting doesn't work yet

--- Week1/Ex4/decypher.py
+++ Week1/Ex4/decypher.py
@@ -1,7 +1,13 @@
 import sys, numpy, os
 
+allHist = []
 text = ""
 stdDev = {}
+highestSTD = 0
+probableLen = 0
+probableKey = ""
+decryptedText = ""
+keyShift = []
 A_INT = ord('a')
 
 # Put encrypted file in string text
@@ -31,7 +37,7 @@ for i in range(5,16) :
 		# TODO : CALCULATE IF THE FOLLOWING CALCULATIONS ARE CORRECT!! 
 		letterChar = text[letter]
 		letterNumber = ord(letterChar)
-		modLetter = (letter + 1) % i
+		modLetter = letter % i
 
 		letterPos = letterNumber - A_INT
 
@@ -44,10 +50,43 @@ for i in range(5,16) :
 
 	# Add std sum to dictionary
 	stdDev.update({i : sumOfStd})
+	allHist.append(lettersHist)
 
 # Print standard deviation sums
 for stdPos in stdDev:
+	if int(stdDev[stdPos]) > highestSTD:
+		highestSTD = stdDev[stdPos]
+		probableLen = stdPos
 	print("Sum of standard deviation length ", stdPos, " = " ,stdDev[stdPos])
+
+# crack the key
+keywordHist = allHist[probableLen-5]
+for i in range(0, probableLen):
+	probE = 0
+	probESize = 0
+	
+	for letter in range(0,26):
+		if keywordHist[i][letter] > probESize:
+			probESize = keywordHist[i][letter]
+			probE = letter + A_INT
+	
+	letter = probE - 4
+	keyShift.append(letter - A_INT)
+	probableKey += chr(letter)
+
+print("The probable key is: %s" % probableKey)
+print(keyShift)
+
+# Decrypt
+for letter in range(0, len(text)):
+	modLetter = letter % probableLen
+	decryptLetter = ((ord(text[letter]) - A_INT) + keyShift[modLetter] ) % 26
+	decryptedText += chr(decryptLetter + A_INT)
+	#print(chr(decryptLetter))
+
+print(decryptedText)
+
+
 
 
 
